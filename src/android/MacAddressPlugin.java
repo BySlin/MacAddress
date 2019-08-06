46a159447067b1747751d01ef0e6036a24db7519
郭亮 <2292735470@qq.com>
2019-08-06 15:23:56 +0800

    add eth0 macAddress

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -2,17 +2,14 @@ package com.badrit.MacAddress;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
-import android.os.Build;
-
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -20,116 +17,63 @@ import java.util.List;
  */
 public class MacAddressPlugin extends CordovaPlugin {
 
-    public boolean isSynch(String action) {
-        if (action.equals("getMacAddress")) {
-            return true;
-        }
-        return false;
-    }
-
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.apache.cordova.api.Plugin#execute(java.lang.String,
      * org.json.JSONArray, java.lang.String)
      */
     @Override
     public boolean execute(String action, JSONArray args,
-            CallbackContext callbackContext) {
+                           CallbackContext callbackContext) {
 
         if (action.equals("getMacAddress")) {
-
-            String macAddress = this.getMacAddress();
-
+            JSONObject macAddress = this.getMacAddress();
             if (macAddress != null) {
-                JSONObject JSONresult = new JSONObject();
-                try {
-                    JSONresult.put("mac", macAddress);
-                    PluginResult r = new PluginResult(PluginResult.Status.OK,
-                            JSONresult);
-                    callbackContext.success(macAddress);
-                    r.setKeepCallback(true);
-                    callbackContext.sendPluginResult(r);
-                    return true;
-                } catch (JSONException jsonEx) {
-                    PluginResult r = new PluginResult(
-                            PluginResult.Status.JSON_EXCEPTION);
-                    callbackContext.error("error");
-                    r.setKeepCallback(true);
-                    callbackContext.sendPluginResult(r);
-                    return true;
-                }
+                callbackContext.success(macAddress);
             }
         }
         return false;
     }
 
-    /**
-     * Gets the mac address.
-     * 
-     * @return the mac address
-     */
-    private String getMacAddress() {
-
-        if (Build.VERSION.SDK_INT >= 23) { // Build.VERSION_CODES.M
-            return getMMacAddress();
-        }
-        
-        return getLegacyMacAddress();
-
-    }
-
-    /**
-     * Gets the mac address on version < Marshmallow.
-     * 
-     * @return the mac address
-     */
-    private String getLegacyMacAddress() {
-
-        String macAddress = null;
-
-        WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        macAddress = wm.getConnectionInfo().getMacAddress();
-
-        if (macAddress == null || macAddress.length() == 0) {
-            macAddress = "02:00:00:00:00:00";
-        }
-
-        return macAddress;
-
-    }
 
     /**
      * Gets the mac address on version >= Marshmallow.
-     * 
+     *
      * @return the mac address
      */
-    private String getMMacAddress() {
-
+    private JSONObject getMacAddress() {
         try {
             List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            JSONObject jsonObject = new JSONObject();
 
             for (NetworkInterface nif : all) {
-                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+                for (Enumeration<InetAddress> enumIpAddr = nif.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (inetAddress instanceof Inet4Address) {
+                        if ("eth0".equals(nif.getName()) || "wlan0".equals(nif.getName())) {
+                            jsonObject.put("ip", inetAddress.getHostAddress());
+                            byte[] macBytes = nif.getHardwareAddress();
+                            if (macBytes == null) {
+                                jsonObject.put("mac", null);
+                            }
 
-                byte[] macBytes = nif.getHardwareAddress();
-                if (macBytes == null) {
-                    return "";
+                            StringBuilder res1 = new StringBuilder();
+                            for (byte b : macBytes) {
+                                res1.append(String.format("%02x", (b & 0xFF)) + ":");
+                            }
+
+                            if (res1.length() > 0) {
+                                res1.deleteCharAt(res1.length() - 1);
+                            }
+                            jsonObject.put("mac", res1.toString());
+                            return jsonObject;
+                        }
+                    }
                 }
-
-                StringBuilder res1 = new StringBuilder();
-                for (byte b : macBytes) {
-                    res1.append(String.format("%02x", (b & 0xFF)) + ":");
-                }
-
-                if (res1.length() > 0) {
-                    res1.deleteCharAt(res1.length() - 1);
-                }
-
-                return res1.toString();
             }
-        } catch (Exception ex) { }
-
-        return "02:00:00:00:00:00";
+        } catch (Exception ex) {
+        }
+        return null;
     }
 }
